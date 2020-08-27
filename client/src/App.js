0596f7ce1f3c4e08f88b01a135125833d9b12b83
AnelRaSant <anel.rmrz.stgo7@gmail.com>
2020-08-27 00:13:38 -0500

    added the full stack app project structure to start to update it according to the generationart app needs

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Books from "./pages/Books";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
+import { BrowserRouter } from "react-router-dom";
+
+// The app will not render correctly until you setup a Route component.
+// Refer to the Basic Example documentation if you need to.
+// (https://reacttraining.com/react-router/web/example/basic)
+function App() {
+  return (
+    <BrowserRouter>
+    <div>
+      <Nav />
+      <Books />
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
