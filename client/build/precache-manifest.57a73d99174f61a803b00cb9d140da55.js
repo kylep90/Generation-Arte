d2eb482fda671cdd5f04f045295f86fe8d37b276
Kyle Pingue <pinguek@gmail.com>
2020-08-27 22:07:29 -0500

    Added functionality to navbar & reactified some html

---
+++ client/build/precache-manifest.57a73d99174f61a803b00cb9d140da55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "48a0ea21a698a70751db",
+    "url": "/static/css/main.1b7fb394.chunk.css"
+  },
+  {
+    "revision": "48a0ea21a698a70751db",
+    "url": "/static/js/main.3adf26fa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "775e1461b8b9e32f6722",
+    "url": "/static/js/2.5561142a.chunk.js"
+  },
+  {
+    "revision": "1b73ba8d5b78e567ae0f89d43fa080bd",
+    "url": "/index.html"
+  }
+];
---
+++ client/src/pages/WhatsOn.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function WhatsOn (){
+    return (
+        <div>
+            <h1>WHATS ON PAGE</h1>
+        </div>
+    )
+}
+export default WhatsOn
